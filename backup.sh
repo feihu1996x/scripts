ef593801b302dbe2c852b46e70a91914dc5fb829
gaosx <gsx0312@qq.com>
2020-08-08 15:37:57 +0800

    update

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# @file: backup.sh
+# @brief: 定时备份重要文件夹
+# @author: feihu1996.cn
+# @date: 20-14-41
+# @version: 1.0
+
+cp -rfv /home/gaosx/Downloads/* /mnt/usb/downloads/
+cp -rfv /opt/app/* /mnt/usb/code/
