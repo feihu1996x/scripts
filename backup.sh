d9b7b1e3cf2dc0c7c981e0460b60a03e64f13a19
gaosx <gsx0312@qq.com>
2020-08-10 10:17:39 +0800

    update backup scripts

--- backup.sh
+++ backup.sh
@@ -6,6 +6,11 @@
 # @date: 20-14-41
 # @version: 1.0
 
-cp -rfv /home/gaosx/Downloads/* /mnt/usb/downloads/
-cp -rfv /opt/app/* /mnt/usb/code/
-cp -rfv /home/gaosx/Pictures/* /mnt/usb/downloads/
+# 备份下载目录
+zip -q -r /tmp/downloads.zip /home/gaosx/Downloads && mv -f /tmp/downloads.zip /mnt/usb/backup/ && rm -f /tmp/downloads.zip
+
+# 备份图片目录
+zip -q -r /tmp/pictures.zip /home/gaosx/Pictures && mv -f /tmp/pictures.zip /mnt/usb/backup/ && rm -f /tmp/pictures.zip
+
+# 备份应用目录
+zip -q -r /tmp/app.zip /opt/app && mv -f /tmp/app.zip /mnt/usb/backup/ && rm -f /tmp/app.zip
