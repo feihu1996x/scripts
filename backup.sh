afc48d61e55200db9b5eb67989bfb90b19c2c31e
gaosx <gsx0312@qq.com>
2020-08-12 08:33:10 +0800

    update

--- backup.sh
+++ backup.sh
@@ -6,19 +6,20 @@
 # @date: 20-08-09
 # @version: 1.0
 
-mkdir -p /mnt/usb/backup/
+BASEDIR=`dirname ${0}`
 
 # 备份下载目录
-zip -q -r /tmp/downloads.zip /home/gaosx/Downloads && mv -f /tmp/downloads.zip /mnt/usb/backup/
+sudo zip -q -r $BASEDIR/downloads.zip /home/gaosx/Downloads && sudo mv -f $BASEDIR/downloads.zip /mnt/usb/backup/
 
 # 备份图片目录
-zip -q -r /tmp/pictures.zip /home/gaosx/Pictures && mv -f /tmp/pictures.zip /mnt/usb/backup/
+sudo zip -q -r $BASEDIR/pictures.zip /home/gaosx/Pictures && sudo mv -f $BASEDIR/pictures.zip /mnt/usb/backup/
 
 # 备份应用目录
-zip -q -r /tmp/app.zip /opt/app && mv -f /tmp/app.zip /mnt/usb/backup/
+sudo zip -q -r $BASEDIR/app.zip /opt/app && sudo mv -f $BASEDIR/app.zip /mnt/usb/backup/
 
 # 备份文档目录
-zip -q -r /tmp/documents.zip /home/gaosx/Documents && mv -f /tmp/documents.zip /mnt/usb/backup/
+sudo zip -q -r $BASEDIR/documents.zip /home/gaosx/Documents && sudo mv -f $BASEDIR/documents.zip /mnt/usb/backup/
 
 # 备份视频目录
-zip -q -r /tmp/videos.zip /home/gaosx/Videos && mv -f /tmp/videos.zip /mnt/usb/backup/
+sudo zip -q -r $BASEDIR/videos.zip /home/gaosx/Videos && sudo mv -f $BASEDIR/videos.zip /mnt/usb/backup/
+
