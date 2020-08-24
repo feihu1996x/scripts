380f5e95c628e22764a0e21fa772af9ee2e9c550
gaosx <gsx0312@qq.com>
2020-08-24 18:40:50 +0800

    update

--- generateExcel.py
+++ generateExcel.py
@@ -12,6 +12,9 @@
 import xlwt
 import json
 import re
+import sys
+
+sys.setrecursionlimit(1000000)
 
 
 style = xlwt.easyxf('align: vertical center, horizontal center')
@@ -19,34 +22,60 @@ appKmCatalog = None
 appKmCatalogMap = {}
 appKmCatalogPidMap = {}
 appKmCatalogClass = {}
+appKmCatalogClassLayer = {}
 outerHeads = []
-sourceCatagory = input("请输入源分类文件:")
-targetFile = input("请输入目标文件:")
 
 
-def setClassKV(_c, _key, _k):
-    _c = _c.get(_key)
-    if not len(_c.keys()):
-        _c[_k] = {}
-        return
-    setClassKV(_key, _k)
+def getMaxDepth():
+    _maxDepth = 1
+    for (_k, _v) in appKmCatalogClassLayer.items():
+        if _v >_maxDepth:
+            _maxDepth = _v
+    return _maxDepth
 
 
-def getAppKmCatagoryByPid(pid):
-    if not appKmCatalogPidMap.get(pid):
-        return
-    for _i in range(len(appKmCatalogPidMap.get(pid))):
-        _k = appKmCatalogPidMap.get(pid)[_i].get("_id")
-        setClassKV(appKmCatalogClass, pid, _k)
+def getRootKey(k):
+    exitFlag = False
+    for (_k, _v) in appKmCatalogClass.items():
+        _currentRootKey = _k
+        while not exitFlag:
+            for (_k1, _v1) in _v.items():
+                if _k1 == k:
+                    exitFlag = True
+                    del _k1
+                    del _v1
+                    break
+                del _k1
+                del _v1
+        if exitFlag:
+            del _k
+            del _v
+            break               
         del _k
-        getAppKmCatagoryByPid(appKmCatalogPidMap.get(pid)[_i].get("_id"))
-        del _i
+        del _v
+        return _currentRootKey
+
+
+def setKV(cl):
+    for (_k, _v) in cl.items():
+        # if getRootKey(_k) not in appKmCatalogClassLayer:
+        #     appKmCatalogClassLayer[getRootKey(_k)] = 0
+        # appKmCatalogClassLayer[getRootKey(_k)] = appKmCatalogClassLayer[getRootKey(_k)] + 1        
+        _v1 = appKmCatalogPidMap.get(_k)
+        if _v1 is None:
+            continue
+        for _i in range(len(_v1)):
+            _v[_v1[_i].get("_id")] = {}
+            setKV(_v)
+            del _i
+        del _k
+        del _v
 
 
 if "__main__" == __name__:
     # wBook1 = xlwt.Workbook()
     # wSheet1 = wBook1.add_sheet("sheet1")
-    with open(sourceCatagory, "r") as _f:
+    with open("/home/gaosx/Downloads/实地地产知识库/app_km_catalog.json", "r") as _f:
         _content = re.sub("}\n{", "},{", _f.read())
         _content = "[" + _content + "]"
         appKmCatalog = json.loads(_content)
@@ -62,10 +91,9 @@ if "__main__" == __name__:
         if appKmCatalog[_j].get("pid") == "0":
             appKmCatalogClass[appKmCatalog[_j].get("_id")] = {}
         del _j
-    for (_k, _v) in appKmCatalogClass.items():
-        getAppKmCatagoryByPid(_k)
-        del _k
-        del _v
-    print(appKmCatalogClass)   
+    setKV(appKmCatalogClass)
+    with open("res.json", "wb") as _f:
+        _f.write(json.dumps(appKmCatalogClass).encode("utf-8"))
+        del _f
+    print(getRootKey("e6e0c0c7-a254-4b88-8d88-4b8f28d8ac01"))
     # wBook.save(targetFile)
-
